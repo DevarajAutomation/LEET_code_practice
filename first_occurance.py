405c45d5aa073b500d3714ac0bfb65b691260b96
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-12-05 16:18:27 +0000

    05-12-2024

--- first_occurance.py
+++ first_occurance.py
@@ -1,15 +1,15 @@
 def needle_haystick(haystack, needle):
     m,n = len(haystack),len(needle)
-
+    print(m)
+    print(n)
     for i in range(m-n+1):
         if haystack[i:i+n] == needle:
             return i
+            
         
     return -1
 
 
-
-
-haystack = "leetcode"
-needle = "leeto"
+haystack = "sadbutsad"
+needle = "sad"
 print(needle_haystick(haystack,needle))
