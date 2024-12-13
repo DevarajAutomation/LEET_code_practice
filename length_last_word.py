7f88e2b4d10ed40661d405b58707ccf75b56afa9
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-12-13 09:45:07 +0000

    13-12-2024

---
+++ length_last_word.py
@@ -0,0 +1,13 @@
+def findLength(s):
+    count = 0
+    flag = False
+    for i in range(len(s) - 1, -1, -1):
+        if ((s[i] >= 'a' and s[i] <= 'z') or (s[i] >= 'A' and s[i] <= 'Z')):
+            flag = True
+            count += 1
+        elif (flag == True):
+            return count
+    return count
+
+s = "luffy is still joyboy"
+print(findLength(s))
---
+++ reverse_string.py
@@ -0,0 +1,8 @@
+def reverse_string(s):
+
+    return s[::-1]
+
+
+st='hello'
+
+print(reverse_string(st))
